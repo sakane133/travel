b6b4c9a8bb581357db5e8e3efbdd40252052baea
github username <s.kane133@gmail.com>
2019-12-01 22:24:16 -0500

    commiting

--- index.js
+++ index.js
@@ -9,30 +9,33 @@ function getTravelersFlightInfo() {
   let profiles = profile.get()
   let airlines = airline.get()
   let trips = trip.get()
+
   let travelers = []
 
   let returnValue = airlines.then(airlineValue => {
     let tripReturn = trips.then(tripValue => {
       airlineValue.airlines.forEach(line => {
+        // creating an object with airline abbreviations and names
         let airObj = {}
         airObj[line.code] = line.name
       })
       let profileReturn = profiles.then(profileValue => {
         profileValue.profiles.forEach(value => {
           let flightArray = []
-          let newHash = {}
           tripValue.trip.flights.forEach(part => {
             if (part.travelerIds.includes(value.personId)) {
-              let flights = { legs: part.legs }
-              flightArray.push(flights)
+              // creating the fligt array for each traveler
+              flightArray.push({ legs: part.legs })
               // Flight array also needs to include airline name and rewards info if applicable
               // will need another if statement for the airline rewards 
             }
           })
+          // creating the hash for each traveler to include Id, Name, and Flight info
+          let newHash = {}
           newHash["id"] = value.personId
           newHash["name"] = value.name
           newHash["flights"] = flightArray
-
+          // adding each traveler hash to the travelers array
           travelers.push(newHash)
         })
         return JSON.stringify(travelers)
@@ -46,7 +49,7 @@ function getTravelersFlightInfo() {
 
 }
 
-// resolving promises for the final hash with travler info
+// resolving promises for the final hash with travler info to return the hash
 
 
 Promise.resolve(getTravelersFlightInfo()).then(console.log)
